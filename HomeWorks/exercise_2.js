69fb2c9b330a4c1fd805a441973e94fe9c9f6106
yavetisyan <y.avetisyan88@gmail.com>
2021-05-18 16:31:03 +0400

    add homeworks

---
+++ HomeWorks/exercise_2.js
@@ -0,0 +1,15 @@
+let a = +prompt("Enter numer a");
+let b = +prompt("Enter number b");
+
+let result;
+
+result = a > b;
+alert(`${a} > ${b} = ${result}`);
+result = a + b;
+alert(`${a} + ${b} = ${result}`);
+result = a * b;
+alert(`${a} * ${b} = ${result}`);
+result = a / b;
+alert(`${a} / ${b} = ${result}`);
+result = a % b;
+alert(`${a} % ${b} = ${result}`);
